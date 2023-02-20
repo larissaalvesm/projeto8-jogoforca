d58e22070692e31fb0e5f9b893c70c1bdfa8b012
larissa <larissaalves1102@gmail.com>
2023-02-20 19:39:54 -0300

    ao clicar numa letra que a palavra contém, ela aparece, caso contrário, o número de erros aumenta e a forca muda a imagem

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import forca0 from "./assets/forca0.png"
+import forca1 from "./assets/forca1.png"
+import forca2 from "./assets/forca2.png"
+import forca3 from "./assets/forca3.png"
+import forca4 from "./assets/forca4.png"
+import forca5 from "./assets/forca5.png"
+import forca6 from "./assets/forca6.png"
 import Letras from "./Letras";
 
 function Jogo(props) {
@@ -7,37 +13,81 @@ function Jogo(props) {
     const { palavras } = props;
 
     const [habilitarLetra, setHabilitarLetra] = useState("letra-desabilitada");
+    const [palavraSorteada, setPalavraSorteada] = useState("");
     const [palavra, setPalavra] = useState([]);
-
-    function comparador() {
-        return Math.random() - 0.5;
-    }
-
+    const [numErros, setNumErros] = useState(0);
+    const [imgForca, setImgForca] = useState(forca0);
 
     function escolherPalavra() {
+
         setHabilitarLetra("letra-habilitada");
-        palavras.sort(comparador);
-        console.log(palavras[0]);
-        const palavraSorteada = [];
 
-        for (let i = 0; i < palavras[0].length; i++) {
-            palavraSorteada.push("_ ");
+        const indiceAleatorio = Math.floor(Math.random() * palavras.length);
+        const novaPalavra = palavras[indiceAleatorio];
+        setPalavraSorteada(novaPalavra);
+
+        const palavraSorteadaEscondida = [];
+
+        for (let i = 0; i < novaPalavra.length; i++) {
+            palavraSorteadaEscondida.push("_ ");
         }
+        setPalavra(palavraSorteadaEscondida);
+    }
 
-        setPalavra(...palavra, palavraSorteada);
+    function letraSelecionada(letra) {
 
+        const letraMin = letra.toLowerCase();
+        const palavraPreenchida = [];
+
+        if (palavraSorteada.includes(letraMin)) {
+            for (let i = 0; i < palavraSorteada.length; i++) {
+                if (palavraSorteada[i] === letraMin) {
+                    palavraPreenchida.push(letraMin);
+                } else if (palavra[i] !== "_ ") {
+                    palavraPreenchida.push(palavraSorteada[i]);
+                } else {
+                    palavraPreenchida.push("_ ");
+                }
+            }
+            setPalavra(palavraPreenchida);
+        } else {
+            let novoNumErros = numErros + 1;
+            let novaImgForca = setarImagemForca(novoNumErros);
+
+            console.log(novaImgForca);
+
+            setNumErros(novoNumErros);
+            setImgForca(novaImgForca);
+        }
+    }
+
+    function setarImagemForca(erros) {
+        switch (erros) {
+            case 1:
+                return forca1;
+            case 2:
+                return forca2;
+            case 3:
+                return forca3;
+            case 4:
+                return forca4;
+            case 5:
+                return forca5;
+            case 6:
+                return forca6;
+        }
     }
 
     return (
         <div className="container">
             <div className="container-superior">
-                <img src={forca0} alt="forca-inicial" />
+                <img src={imgForca} alt="forca-inicial" />
                 <div className="container-direita">
-                    <button onClick={escolherPalavra}>Escolher Palavra</button>
+                    <button disabled={palavra.length === 0 ? false : true} onClick={escolherPalavra}>Escolher Palavra</button>
                     <div className="palavra-escolhida">{palavra}</div>
                 </div>
             </div>
-            <Letras habilitarLetra={habilitarLetra} />
+            <Letras habilitarLetra={habilitarLetra} letraSelecionada={letraSelecionada} />
         </div>
     )
 }
--- src/Letras.js
+++ src/Letras.js
@@ -1,10 +1,19 @@
 function Letras(props) {
 
+    const { habilitarLetra, letraSelecionada } = props;
+
     const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     return (
         <div className="container-inferior">
-            {alfabeto.map((letra) => <button type="button" disabled className={props.habilitarLetra}>{letra}</button>)}
+            {alfabeto.map((letra) =>
+                <button
+                    disabled={habilitarLetra === "letra-desabilitada" ? true : false}
+                    className={habilitarLetra}
+                    onClick={() => letraSelecionada(letra)}
+                >
+                    {letra}
+                </button>)}
         </div>
     )
 }
